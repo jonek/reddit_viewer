9c92ea44b0127d9fe028860db9d0fa1bbb9dc699
Johannes Eickhold <jeick@eclipsesource.com>
2019-05-08 20:42:57 +0000

    Use correct PATH in Gitpod startup script

--- .gitpod-devServer.sh
+++ .gitpod-devServer.sh
@@ -1,5 +1,9 @@
 export URL=$(gp url 8080 | cut -d \/ -f 1-3)
+export PATH=$PATH:$(pwd)/node_modules/.bin
 
 qrcode ${URL}
 echo Public URL of dev server is: ${URL}
+
+$(sleep 5 && gp preview $(gp url 8080)package.json)
+
 tabris serve -l
